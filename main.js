a3ef865a0d5a6b04795c607a3543a5c777d4deb1
11a2thd <127629180+11a2thd@users.noreply.github.com>
2023-03-12 06:31:25 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ function Mua(){
 function login(){
 	var userName = document.getElementById("user_name").value
 	var passWord = document.getElementById("user_password").value
-	if(userName == 'ngocnhatbruh' && passWord == "Ngoc123456")
-	confirm('Đăng nhập thành công.')
+	if(userName == 'ngocnhatbruh' && passWord == "Ngoc123456"){
+		login.style.display = 'none'
+		home.style.display = 'block'
+		
+	}
 	else confirm('Đăng nhập không thành công.')
-	console.log(typeof userName)
-	console.log(userName)
-	console.log(typeof passWord)
-	console.log(passWord)
 }
