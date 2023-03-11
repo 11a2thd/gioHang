ca129998ef484fd34feac1c5918fbb8c68112d31
11a2thd <127629180+11a2thd@users.noreply.github.com>
2023-03-12 03:40:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function Mua(){
+	confirm('Có cái đặc cầu!!!')
+}
+
+Mua.onclick = function() {
+        Mua()
+}
+function Mua() {
+    confirm('Có cái đặc cầu!!!')
+}
+
+function test(text){
+	confirm(text)
+}
+
+var _object = {
+	ten: 'Ngoc',
+	tuoi: 'Lon',
+	nam: 2003
+}
+console.log(_object.ten)
+console.log(_object)
