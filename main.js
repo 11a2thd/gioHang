d649547740f432064f6b906111931004b27842b7
11a2thd <127629180+11a2thd@users.noreply.github.com>
2023-03-12 19:40:44 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+// const Login = document.querySelector('.Login');
+// const home = document.querySelector('.home');
+// const button_login = document.querySelector('.button_login');
+
 function Mua(){
 	confirm('Có cái đặc cầu!!!')
+}
+
+function login1(){
+	document.querySelector('.home').style.display = 'none';
+	document.querySelector('.Login').style.display = 'block';
 }
 
 function login(){
 	var userName = document.getElementById("user_name").value
 	var passWord = document.getElementById("user_password").value
 	if(userName == 'ngocnhatbruh' && passWord == "Ngoc123456"){
-		login.style.display = 'none'
-		home.style.display = 'block'
-		
+		document.querySelector('.home').style.display = 'block';
+		document.querySelector('.Login').style.display = 'none';
+		document.querySelector('.sign').style.display = 'none';
+		document.querySelector('.logOut').style.display = 'block';
 	}
-	else confirm('Đăng nhập không thành công.')
+	else confirm('Có cái tài khoản/mật khẩu cũng đéo nhớ được.')
+}
+
+function logOut(){
+	document.querySelector('.logOut').style.display = 'none';
+	document.querySelector('.sign').style.display = 'block';
+	document.querySelector('.home').style.display = 'block';
+}
+
+function test(){
+	
 }
