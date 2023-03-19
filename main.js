cac44120371926847247474ece7c96d2a3030991
11a2thd <127629180+11a2thd@users.noreply.github.com>
2023-03-20 05:05:27 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,10 @@
 // const button_login = document.querySelector('.button_login');
 const admin_btn = document.querySelector('.admin_btn');
 const user_btn = document.querySelector('.user_btn');
-const mua_btn = Object.values(document.querySelectorAll('.mua_btn'))
-
-var mua = mua_btn.map(function(mua){
-	mua.onclick = function (){
-		confirm('Có cái con cặc!!!')
-	}
-})
+const ad = document.querySelector('.ad');
+const insert = document.querySelector('.insert');
+const button_insert = document.querySelector('.button_insert');
+const button_erase = document.querySelector('.button_erase');
 
 function login1(){
 	document.querySelector('.home').style.display = 'none';
@@ -29,38 +26,64 @@ function login(){
 }
 
 admin_btn.onclick = function (){
-	var mua1 = mua_btn.map(function(mua){
+	var mua_btn = Object.values(document.querySelectorAll('.mua_btn'))
+	var mua = mua_btn.map(function(mua){
 		return mua.style.display = 'none'
 	})
-	document.querySelector('.admin_btn').style.display = 'none';
-	document.querySelector('.ad').style.display = 'block';
-	document.querySelector('.user_btn').style.display = 'block';
+	admin_btn.style.display = 'none';
+	ad.style.display = 'block';
+	user_btn.style.display = 'block';
 }
 
 user_btn.onclick = function (){
-	var mua1 = mua_btn.map(function(mua){
+	var mua_btn = Object.values(document.querySelectorAll('.mua_btn'))
+	var mua = mua_btn.map(function(mua){
 		return mua.style.display = 'block'
 	})
-	document.querySelector('.admin_btn').style.display = 'block';
-	document.querySelector('.ad').style.display = 'none';
-	document.querySelector('.user_btn').style.display = 'none';
-	document.querySelector('.insert').style.display = 'none';
+	admin_btn.style.display = 'block';
+	ad.style.display = 'none';
+	user_btn.style.display = 'none';
+	insert.style.display = 'none';
+	var mua = mua_btn.map(function(mua){
+		mua.onclick = function (){
+			confirm('Có cái con cặc!!!')
+		}
+	})
+	var xoa_btn = Object.values(document.querySelectorAll('.xoa_btn'))
+	var xoa = xoa_btn.map(function(xoa){
+		return xoa.style.display = 'none'
+	})
 }
 
-function insert(){	
-	document.querySelector('.insert').style.display = 'block';
-}
-
-function erase(){
-	document.querySelector('.insert').style.display = 'block';
+button_insert.onclick = function (){
+	insert.style.display = 'block';
+	var xoa_btn = Object.values(document.querySelectorAll('.xoa_btn'))
+	var xoa = xoa_btn.map(function(xoa){
+		return xoa.style.display = 'none'
+	})
 }
 
 function edit(){
 	document.querySelector('.insert').style.display = 'block';
 }
 
+const arrs = []
+
+var link_anh = document.getElementsByClassName('link_anh')
+var ten_sp = document.getElementsByClassName('ten_sp')
+var so_luong = document.getElementsByClassName('so_luong')
+var gia = document.getElementsByClassName('gia')
+
+for(var i = 0; i < ten_sp.length; i++){
+	arrs.push({
+		link_anh: link_anh[i].src,
+		ten_sp: ten_sp[i].innerHTML,
+		so_luong: so_luong[i].innerHTML,
+		gia: gia[i].innerHTML,
+	})
+}
+
 function them(){
-	var arrs = []
 	if(document.getElementById("ten_sp").value === '' || 
 	document.getElementById("link_anh").value === '' || 
 	document.getElementById("so_luong").value === '' || 
@@ -69,31 +92,54 @@ function them(){
 	}
 	else{
 		arrs.push({
-			ten: `${document.getElementById("ten_sp").value}`,
-			link: `${document.getElementById("link_anh").value}`,
+			link_anh: `${document.getElementById("link_anh").value}`,
+			ten_sp: `${document.getElementById("ten_sp").value}`,
 			so_luong: `${document.getElementById("so_luong").value}`,
 			gia: `${document.getElementById("gia").value}`
 		})
 	}
-	them1(arrs)	
-	document.getElementById("ten_sp").value = ""
+	them1(arrs[arrs.length - 1])
+	console.log(arrs)
 	document.getElementById("link_anh").value = ""
+	document.getElementById("ten_sp").value = ""
 	document.getElementById("so_luong").value = ""
 	document.getElementById("gia").value = ""	
 }
 
 function them1(arrs){
-	const html = arrs.map(function(arrs, index){
-		return document.getElementById("san_pham").innerHTML += `
-																<div align="center" class="sanPham">
-                    <img src="${arrs.link}" alt="${arrs.ten}" class="img_sp">
-                    <div class="thong_tin">
-                        <h3>${arrs.ten}</h3>
-                        <p>Số lượng: ${arrs.so_luong}</p>
-                        <p>Giá: ${arrs.gia} đ</p>
-                        <button class="mua_btn" style="display: none;">Mua hàng</button>
-                    </div>
-                </div>`
+		return document.getElementById("san_pham").insertAdjacentHTML('beforeend', `<div class="sanPham" align="center">
+		<img src="${arrs.link_anh}" alt="${arrs.ten_sp}" class="img_sp link_anh">
+		<div class="thong_tin">
+			<h3 class="ten_sp">${arrs.ten_sp}</h3>
+			<p class="so_luong">Số lượng: ${arrs.so_luong}</p>
+			<p class="gia">Giá: ${arrs.gia} đ</p>
+			<button class="mua_btn" style="display: none;">Mua hàng</button>
+			<button class="xoa_btn" style="display: none;">Xóa hàng</button>
+		</div>
+	</div>`) 
+}
+
+button_erase.onclick = function (){
+	document.querySelector('.insert').style.display = 'none';
+	var mua_btn = Object.values(document.querySelectorAll('.mua_btn'))
+	var mua = mua_btn.map(function(mua){
+		return mua.style.display = 'none'
+	})
+	var xoa_btn = Object.values(document.querySelectorAll('.xoa_btn'))
+	var xoa = xoa_btn.map(function(xoa){
+		return xoa.style.display = 'block'
+	})
+	return xoa_hang()
+}
+
+function xoa_hang(){
+	var arr = Object.values(document.querySelectorAll('.xoa_btn'))
+	console.log(arr)
+	var xoa1 = arr.map(function(arr, index){
+		arr.onclick = function (){
+			document.getElementsByClassName('sanPham')[index].remove()
+			return xoa_hang()
+		}	
 	})
 }
 
